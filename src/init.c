8be2c75d010b5fba27d1aeb49d31a67ba87b89c2
Pierre-Hugues Husson <phhusson@gmail.com>
2013-07-14 16:40:32 +0200

    Actual code for clock init

--- src/init.c
+++ src/init.c
@@ -1,6 +1,10 @@
+#include <stm32f4xx.h>
+
 void clock_init(void) {
 	//HSEON&~HSEBYP
+	RCC->CR = (RCC->CR | RCC_CR_HSEON) & ~RCC_CR_HSEBYP;
 	//Wait for HSERDY
+	while( !(RCC->CR&RCC_CR_HSERDY) );
 
 	//cf p125 for clocks derivation
 	//We want main clock @168MHz, and USG OTG FS @48MHz
@@ -15,9 +19,17 @@ void clock_init(void) {
 
 	// real registers values:
 	// PLLQ=7, PLLSRC=1, PLLP=0, PLLN=28, PLLM=2
+	RCC->PLLCFGR =
+		/* PLLQ */ 7 << 24 |
+		/* PLLSRC */ 1 << 22 |
+		/* PLLP */ 0 << 16 |
+		/* PLLN */ 28 << 6 |
+		/* PLLM */ 2 << 0;
 
 	//PLLON
+	RCC->CR = RCC->CR | RCC_CR_PLLON;
 	//Wait for PLLRDY
+	while( !(RCC->CR & RCC_CR_PLLRDY) );
 	
 	//Now let's use PLL's output as main clock
 	//First setup dividors for AHB/APB1/APB2
@@ -25,8 +37,14 @@ void clock_init(void) {
 	//AHB divisor = 1, HPRE = 0
 	//APB1 divisor = 4, PPRE1=b101=5
 	//APB2 divisor = 2, PPRE2=b100=4
+	RCC->CFGR = ( RCC->CFGR & ~RCC_CFGR_HPRE & ~RCC_CFGR_PPRE1 & ~RCC_CFGR_PPRE2 ) |
+		/* HPRE */ 0 << 4 |
+		/* PPRE1 */ 5 << 10 |
+		/* PPRE2 */ 4 << 13;
 
 	//Then switch to apropriate clock source
 	//SW=2
+	RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_SW) | 2 << 0;
 	//Wait for SWS=2
+	while( (RCC->CFGR & RCC_CFGR_SWS) != (2<<2) );
 }
