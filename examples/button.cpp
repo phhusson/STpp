8d478aef0e8b90e5171e1a58b907a03cef387798
Pierre-Hugues Husson <phh@phh.me>
2014-02-23 22:22:22 +0100

    Button example uses Debug to send messages

--- examples/button.cpp
+++ examples/button.cpp
@@ -1,15 +1,18 @@
 #include <Board.h>
 #include <tr1/functional>
 #include <Log.h>
+#include <Debug.h>
 #include <Exti.h>
 
 extern "C" void vTaskDelay(int);
 int main() {
 	bool v = false;
+			debug << "Hello !" << endl;
 	Exti(UserButton)
 		.enableFalling()
 		.disableRising()
 		.setTopCB([&](int) {
+			debug << "Hello !" << endl;
 			LedR.setDutyCycle(v ? 100 : 0);
 			v = !v;
 		})
