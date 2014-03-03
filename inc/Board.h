cee9e4a341caf09434654f1692a809d3c2dcf5ed
Pierre-Hugues Husson <phh@phh.me>
2014-03-03 01:16:06 +0100

    Sanitize board file/header

--- inc/Board.h
+++ inc/Board.h
@@ -2,45 +2,40 @@
 #include <Pwm.h>
 #include <Time.h>
 #include <HBridgeST.h>
+#include <IncrementalEncoder.h>
 
-extern Pwm LedG;
-extern Pwm LedO;
-extern Pwm LedB;
-extern Pwm LedR;
 
-extern Gpio UserButton;
-
-extern Gpio Lcd_RS;
-extern Gpio Lcd_E;
-extern Gpio Lcd_DB4;
-extern Gpio Lcd_DB5;
-extern Gpio Lcd_DB6;
-extern Gpio Lcd_DB7;
-
-// Motor
+// Motor control
 extern Gpio Prop0_PWM;
 extern Gpio Prop0A;
 extern Gpio Prop0B;
 extern HBridgeST HBridge0;
+
+extern Gpio Enc0A;
+extern Gpio Enc0B;
+extern IncrementalEncoder Encoder0;
 
 extern Gpio Prop1_PWM;
 extern Gpio Prop1A;
 extern Gpio Prop1B;
 extern HBridgeST HBridge1;
 
-// Rotary encoder
-extern Gpio RotaryA;
-extern Gpio RotaryB;
-
-//Drived by on-board logic...
-//You'd better let it that way
-extern Gpio USB_LedG;
-extern Gpio USB_LedR;
-
+//USB
+extern Gpio USB_Vbus_OC;
 extern Gpio USB_Vbus_en;
 extern Gpio USB_Vbus_det;
 extern Gpio USB_ID;
-extern Gpio USB_DP;
 extern Gpio USB_DM;
+extern Gpio USB_DP;
 
+//On-board leds
+extern Pwm LedG;
+extern Pwm LedO;
+extern Pwm LedB;
+extern Pwm LedR;
+
+//Extra
+extern Gpio UserButton;
+
+//Which timer to use for precise (<1ms) timing
 extern Time time;
--- lib/Board.static.h
+++ lib/Board.static.h
@@ -2,6 +2,7 @@
 #include "Led.h"
 #include "Pwm.h"
 #include "HBridgeST.h"
+#include "IncrementalEncoder.h"
 
 // Motor control
 Gpio Prop0_PWM(GpioB[10]);
@@ -9,14 +10,16 @@ Gpio Prop0A(GpioB[13]);
 Gpio Prop0B(GpioB[15]);
 HBridgeST HBridge0(Prop0A, Prop0B, Prop0_PWM, Tim2, 3);
 
+Gpio Enc0A(GpioE[9]);
+Gpio Enc0B(GpioE[11]);
+IncrementalEncoder Encoder0(Enc0A, Enc0B, Tim1, 3);
+
 Gpio Prop1_PWM(GpioB[11]);
 Gpio Prop1A(GpioB[12]);
 Gpio Prop1B(GpioB[14]);
 HBridgeST HBridge1(Prop1A, Prop1B, Prop1_PWM, Tim2, 4);
 
-//Rising edge
-Gpio UserButton(GpioA[0]);
-
+//USB
 Gpio USB_Vbus_OC(GpioD[5]);
 Gpio USB_Vbus_en(GpioC[0]);
 Gpio USB_Vbus_det(GpioA[9]);
@@ -24,19 +27,27 @@ Gpio USB_ID(GpioA[10]);
 Gpio USB_DM(GpioA[11]);
 Gpio USB_DP(GpioA[12]);
 
-Time time(Tim14);
 
-Gpio g_LedG(GpioD[12]);
-Gpio g_LedO(GpioD[13]);
-Gpio g_LedR(GpioD[14]);
-Gpio g_LedB(GpioD[15]);
-
+//On-board leds
 /*
  *      O
  *  G        R
  *      G
  */
+Gpio g_LedG(GpioD[12]);
 Pwm LedG(g_LedG, Tim4, 1);
+
+Gpio g_LedO(GpioD[13]);
 Pwm LedO(g_LedO, Tim4, 2);
+
+Gpio g_LedR(GpioD[14]);
 Pwm LedR(g_LedR, Tim4, 3);
+
+Gpio g_LedB(GpioD[15]);
 Pwm LedB(g_LedB, Tim4, 4);
+
+//Extra
+Gpio UserButton(GpioA[0]);
+
+//Which timer to use for precise (<1ms) timing
+Time time(Tim14);
