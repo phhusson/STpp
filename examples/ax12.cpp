a1108f88a10017031a3ea6ade9de61ec9d614cc8
Pierre-Hugues Husson <phh@phh.me>
2014-05-04 14:13:59 +0200

    New Ax12 api for three wires mode

--- examples/ax12.cpp
+++ examples/ax12.cpp
@@ -11,26 +11,11 @@ int main() {
 	//auto ax12_pin = GpioB[10];
 	//Ax12 ax12_broadcast(ax12_pin, Uart(3, &uartDma), 0xfe);
 	//Enable torque
-
 	auto Ax12Tx = GpioA[2];
 	auto Ax12Rx = GpioA[3];
 	auto Ax12En = GpioA[1];
 
-	Ax12Tx
-		.setPushPull()
-		.setAlternate(Gpio::USART1_3)
-		.setDirection(Gpio::OUTPUT)
-		.setSpeed(Gpio::SPEED_100MHz)
-		.setResistor(Gpio::PULL_UP);
-
-	Ax12Rx
-		.setPushPull()
-		.setAlternate(Gpio::USART1_3)
-		.setDirection(Gpio::INPUT)
-		.setSpeed(Gpio::SPEED_100MHz)
-		.setResistor(Gpio::PULL_UP);
-
-	Ax12 ax12_broadcast(Uart(2), 0xfe, &Ax12En);
+	Ax12 ax12_broadcast(Uart(2), 0xfe, &Ax12En, Ax12Rx, Ax12Tx);
 	ax12_broadcast.setSpeed(0x3ff);
 	ax12_broadcast.enable();
 
