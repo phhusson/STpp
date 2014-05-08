e6b2aadec6a10a9a45c31f743d6c488e42a39e50
Felix Raimundo <felix.raimundo@telecom-paristech.fr>
2014-05-09 00:16:45 +0200

    Attempt at a Doxygen on the Incremental counter

--- lib/IncrementalEncoder.cpp
+++ lib/IncrementalEncoder.cpp
@@ -1,5 +1,15 @@
 #include <IncrementalEncoder.h>
 
+/**
+ * @brief Creates an IncrementalEncoder object
+ *
+ * @params a           Gpio of the A pin of the Incremental Encoder
+ * @params b           Gpio of the B pin of the Incremental Encoder
+ * @params t           Timer on which the encoder's gpios are plugged
+ * @params prescaler   Timer prescaler
+ * @params reverse     Set to 1 of the pins A and B are switched (as the timer has no way of knowing which one is A or B)
+ *
+ */
 IncrementalEncoder::IncrementalEncoder(Gpio& a, Gpio& b, Timer& t, int prescaler, bool reverse):
 	encoderA(a), encoderB(b), tim(t), reverse(reverse) {
 
@@ -17,11 +27,13 @@ IncrementalEncoder::IncrementalEncoder(Gpio& a, Gpio& b, Timer& t, int prescaler
 		.setInputCapture2Filter(0)
 		.setSlaveModeSelection(3)
 		.enable();
+
 	encoderA
 		.setSpeed(Gpio::SPEED_100MHz)
 		.setDirection(Gpio::INPUT)
 		.setResistor(Gpio::PULL_UP);
 	encoderB
+
 		.setSpeed(Gpio::SPEED_100MHz)
 		.setDirection(Gpio::INPUT)
 		.setResistor(Gpio::PULL_UP);
@@ -30,26 +42,33 @@ IncrementalEncoder::IncrementalEncoder(Gpio& a, Gpio& b, Timer& t, int prescaler
 	last_value = 0;
 }
 
+/**
+ * @brief Sets the counter value to the argument value
+ *
+ */
 int IncrementalEncoder::operator=(int v) {
-	if(reverse)
-		v = -v;
-	counter = v;
-	return -counter;
+	counter = reverse ? -v : v;
+	return v;
 }
 
-//Compute absolute coder's position, assuming the previous position is less than half a timer's cycle away
+/**
+ * @brief Computes the absolute coder's position
+ *
+ * @details assuming the previous position is less than half a timer's cycle away
+ *
+ */
 IncrementalEncoder& IncrementalEncoder::update() {
 	int v = tim.getCounter();
 
-	//Assume we're going forward
+	// Assume we're going forward
 	int d = v - last_value;
 	if(d <= 0)
 		d+=65536;
 
 
-	if(d>=32768) {
-		//We're too far, we were wrong
-		//Meaning we're actally going backward
+	if(d >= 32768) {
+		// We're too far, we were wrong
+		// Meaning we're actally going backward
 		d = d - 65536;
 	}
 	counter += d;
@@ -58,6 +77,10 @@ IncrementalEncoder& IncrementalEncoder::update() {
 	return *this;
 }
 
+/**
+ * @brief Gives out the current absolute value of the counter
+ *
+ */
 int IncrementalEncoder::getCounter(){
 	update();
 	if(reverse)
@@ -65,6 +88,10 @@ int IncrementalEncoder::getCounter(){
 	return counter;
 }
 
+/**
+ * @brief Gets the value from the getCounter function call 
+ *
+ */
 IncrementalEncoder::operator int() {
 	return getCounter();
 }
